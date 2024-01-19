5117469b0eb93c5fff458c6ed9b441bc370bb91e
Matheus Melo <matheus.correia@lavid.ufpb.br>
2024-01-19 00:37:12 -0300

    03-dt-money: transactions context

--- classes/03-dt-money/src/pages/Transactions/index.tsx
+++ classes/03-dt-money/src/pages/Transactions/index.tsx
@@ -6,8 +6,12 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from './styles'
+import { TransactionContext } from '../../contexts/TransactionsContext'
+import { useContext } from 'react'
 
 export default function Transactions() {
+  const { transactions } = useContext(TransactionContext)
+
   return (
     <div>
       <Header />
@@ -17,22 +21,20 @@ export default function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburguer</td>
-              <td>
-                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
