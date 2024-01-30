fb638b5bb75a41180b61985a06ce0f2f958d45b6
Matheus Melo <matheus.correia@lavid.ufpb.br>
2024-01-29 21:29:10 -0300

    04-pizza-shop: pagination component

---
+++ classes/04-pizza-shop/src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,45 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+// interface OrderTableRowProps {
+//   idx: string
+// }
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="text-xm font-mono font-medium">
+        tsd8458hj9345sd894sd534589
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Matheus Melo</TableCell>
+      <TableCell className="font-medium">R$ 179,90</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
