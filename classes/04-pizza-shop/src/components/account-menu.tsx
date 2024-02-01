3488582acbe6358b33be762fe882a72a8ef9b03a
Matheus Melo <matheus.correia@lavid.ufpb.br>
2024-01-31 23:34:28 -0300

    04-pizzashop: edit profile form

--- classes/04-pizza-shop/src/components/account-menu.tsx
+++ classes/04-pizza-shop/src/components/account-menu.tsx
@@ -4,7 +4,9 @@ import { Building, ChevronDown, LogOut } from 'lucide-react'
 import { getManagedRestaurant } from '@/api/get-managed-restaurant'
 import { getProfile } from '@/api/get-profile'
 
+import { StoreProfileDialog } from './store-profile-dialog'
 import { Button } from './ui/button'
+import { Dialog, DialogTrigger } from './ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,16 +21,18 @@ export function AccountMenu() {
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
+    staleTime: Infinity,
   })
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
     useQuery({
       queryKey: ['managed-restaurant'],
       queryFn: getManagedRestaurant,
+      staleTime: Infinity,
     })
 
   return (
-    <div>
+    <Dialog>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
@@ -60,16 +64,20 @@ export function AccountMenu() {
             )}
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Building className="mr-2 h-4 w-4" />
-            <span>Perfil da loja</span>
-          </DropdownMenuItem>
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
           <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
             <LogOut className="mr-2 h-4 w-4" />
             <span>Sair</span>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-    </div>
+
+      <StoreProfileDialog />
+    </Dialog>
   )
 }
